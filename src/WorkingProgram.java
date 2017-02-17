7585bd2328a83befcf0464bc7d05512d16eee55a
Czeffik <trzewik93@gmail.com>
2017-02-17 18:10:27 +0100

    x

--- src/WorkingProgram.java
+++ src/WorkingProgram.java
@@ -1,19 +1,25 @@
 public class WorkingProgram {
     private Long actualTime;
+    private long time =1487354400;
+
     public WorkingProgram() throws InterruptedException {
         WritingMathToFile work = new WritingMathToFile();
         actualTime = (System.currentTimeMillis()/1000);
         this.working();
+
     }
     private void working() throws InterruptedException {
 
-        if (actualTime == 1487354400){
-            Thread.sleep(1000);
+        if ((actualTime=actualTime-(actualTime%100)) == time){
             WritingMathToFile working = new WritingMathToFile();
-            actualTime+=10800;
+            time+=10800;
+            Thread.sleep((time-actualTime)*1000);
+            actualTime = (System.currentTimeMillis()/1000);
+            this.working();
         }
         else{
-            Thread.sleep(1000);
+            actualTime = (System.currentTimeMillis()/1000);
+            Thread.sleep((time-actualTime)*1000);
             this.working();
         }
     }
